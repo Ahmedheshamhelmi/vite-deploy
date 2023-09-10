2cbd3a42eb48f54994c34b2bdae777859eb4e10b
Ahmedheshamhelmi <Ahmedheshamhelmi@users.noreply.github.com>
2023-09-10 22:40:20 +0000

    deploy: 2788a7661d6e28e6230fdc619a5f4408e5be0142

---
+++ assets/images/index.js
@@ -0,0 +1,18 @@
+import ads1 from './ads1.jpg'
+import ads2 from './ads2.webp'
+import ads3 from './ads3.jpg'
+import ads4 from './ads4.jpg'
+
+import dw1 from './dw1.svg'
+import dw2 from './dw2.png'
+import dw3 from './dw3.svg'
+
+import mbanner1 from './main-banner.jpg'
+import mbanner2 from './main-banner-1.jpg'
+import sbanner1 from './catbanner-01.jpg'
+import sbanner2 from './catbanner-02.jpg'
+import sbanner3 from './catbanner-03.jpg'
+import sbanner4 from './catbanner-04.jpg'
+
+export {ads1, ads2, ads3, ads4, dw1, dw2, dw3, mbanner1, mbanner2, sbanner1, sbanner2, sbanner3,sbanner4} 
+
